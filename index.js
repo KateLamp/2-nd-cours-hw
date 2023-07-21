6a1a2343a758c093b505036ba8bd53814d102caf
KateLamp <evka2709@gmail.com>
2023-07-22 00:01:37 +0300

    Изменения в задании 8

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function printMessage() {
     let userAnswer = prompt('Введите номер месяца');
-    if (userAnswer == 1 || 2 || 12) {
+    if (userAnswer == 1 || userAnswer == 2 || userAnswer == 12) {
         console.log('Зима');
-    } else if (userAnswer == 3 || 4 || 5) {
+    } else if (userAnswer == 3 || userAnswer == 4 || userAnswer == 5) {
         console.log('Весна');
-    } else if (userAnswer == 6 || 7 || 8) {
+    } else if (userAnswer == 6 || userAnswer == 7 || userAnswer == 8) {
         console.log('Лето');
-    } else if (userAnswer == 9 || 10 || 11) {
+    } else if (userAnswer == 9 || userAnswer == 10 || userAnswer == 11) {
         console.log('Осень');
     } else {
         console.log('Братьев-месяцев всего двенадцать');
